7fd4832130c429dd54c29e05431f5db66a8d95ae
raulesparr <raulesparzavergara@gmail.com>
2025-10-27 17:54:04 +0100

    corrección ofsets

--- lib/RobotArm/robotArm.cpp
+++ lib/RobotArm/robotArm.cpp
@@ -123,11 +123,11 @@ void RobotArm::processCommandTask(void* pvParameters)
 
 void RobotArm::setupCommand(const char* param, const char* value)
 {
-  int offset1 = 90;
-  int offset2 = 102;
-  int offset3 = 89;
-  int offset4 = 135;
-  int offset5 = 80;
+  int offset1 = 26;
+  int offset2 = 101;
+  int offset3 = 90;
+  int offset4 = 145;
+  int offset5 = 66;
   int offset6 = 0;
 
   Serial.printf("[SETUP] Setting %s to %s\n", param, value);
